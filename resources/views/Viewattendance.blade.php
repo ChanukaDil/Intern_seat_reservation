863d11e68a96ffb488fd0d22a711ccb5fb93c87b
Princely2001 <157102478+Princely2001@users.noreply.github.com>
2024-10-08 20:52:24 +0530

    myfirst commit

--- resources/views/Viewattendance.blade.php
+++ resources/views/Viewattendance.blade.php
@@ -2,30 +2,27 @@
 @extends('layouts.app1')
 
 @section('content')
-<div style="background-color: #1d8b37; ">
-</br>
-</br></br></br></br>
-<form class="px-md-2" action="{{ route('search') }}" method="POST">
-    @csrf
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <h2 class="form-title">Search <strong>Attendance Date</strong></h2>
-            </div>
-            <div class="col-md-9">
-                <div class="input-group">
-                    <input type="date" name="date" class="form-control" required>
-                    <span class="input-group-addon">
-                       &nbsp;&nbsp;<input type="submit" value="Search" class="btn btn-primary">
-                    </span>
+<div style="background-color: #1d8b37;">
+    </br></br></br></br></br>
+    <form class="px-md-2" action="{{ route('search') }}" method="POST">
+        @csrf
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h2 class="form-title">Search <strong>Attendance Date</strong></h2>
+                </div>
+                <div class="col-md-9">
+                    <div class="input-group">
+                        <input type="date" name="date" class="form-control" required max="{{ date('Y-m-d') }}">
+                        <span class="input-group-addon">
+                           &nbsp;&nbsp;<input type="submit" value="Search" class="btn btn-primary">
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-</br>
-</br></br></br></br></br>
-</form>
+    </br></br></br></br></br></br>
+    </form>
 </div>
 </br>
 @endsection
