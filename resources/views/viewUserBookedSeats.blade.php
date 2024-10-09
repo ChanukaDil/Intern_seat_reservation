89132f7f0df8667a52a16edcb46fd2517415ea79
Shynie Peiris <shyniepeiris@gmail.com>
2024-10-09 14:34:16 +0530

    update view user booked seats page

--- resources/views/viewUserBookedSeats.blade.php
+++ resources/views/viewUserBookedSeats.blade.php
@@ -6,39 +6,36 @@
 <h1 class="text-center" style="background-color: #1d8b37;">Your Booked Seats</h1>
 &nbsp;<br/>
 
-<table class="table table-hover">
-    <thead>
-      <tr style="background-color:rgb(188, 121, 121);">
-        <th scope="col">Seat Number</th>
-        <th scope="col">Date</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      @if(isset($bookedSeats) && $bookedSeats->isNotEmpty())
-          @foreach($bookedSeats as $seat)
-          <tr>
-            <td>{{ $seat->seat_number }}</td>
-            <td>{{ $seat->date }}</td>
-            <td>
-                <form action="{{ route('seats.cancel', $seat->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to cancel this reservation?');">
-                    @csrf
-                    @method('PATCH')
-                    <button type="submit" class="btn btn-danger">Cancel</button>
-                </form>
-            </td>
-          </tr>
-          @endforeach
-      @else
-          <tr>
-            <td colspan="3" class="text-center">You haven't booked any seats yet</td>
-          </tr>
-      @endif
-    </tbody>
-  </table>
-  <button class="btn btn-primary btn-lg btn-block" type="button" onclick="window.location.href='{{ route('res') }}'">
-    Back to Reservation Page
-  </button>
+<div class="row">
+  @if(isset($bookedSeats) && $bookedSeats->isNotEmpty())
+      @foreach($bookedSeats as $seat)
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <div class="card-header" style="background-color: #1d8b37; color: white;">
+            Seat Reservation
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Seat Number: {{ $seat->seat_number }}</h5>
+            <p class="card-text">Date: {{ $seat->date }}</p>
+            <form action="{{ route('seats.cancel', $seat->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to cancel this reservation?');">
+                @csrf
+                @method('PATCH')
+                <button type="submit" class="btn btn-danger">Cancel Reservation</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      @endforeach
+  @else
+      <div class="col-md-12 text-center">
+        <p>You haven't booked any seats yet.</p>
+      </div>
+  @endif
+</div>
+
+<button class="btn btn-primary btn-lg btn-block" type="button" onclick="window.location.href='{{ route('res') }}'">
+  Back to Reservation Page
+</button>
 &nbsp;<br/>
 &nbsp;<br/>
 </div>
