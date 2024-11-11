55e1eb93e2a7bbcac4a362082cbc5882f3a7372f
Princely2001 <157102478+Princely2001@users.noreply.github.com>
2024-11-11 14:57:00 +0530

    update view attendance

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CrudController;
 use App\Http\Controllers\SeatController;
@@ -8,11 +7,8 @@ use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FacebookController;
 use App\Http\Controllers\GoogleAuthController;
-
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/home', [HomeController::class, 'res'])->name('res');
-
 Route::get('login', [HomeController::class, 'login'])->name('login');
 Route::get('Aboutus', [HomeController::class, 'Aboutus'])->name('About');
 Route::get('payment', [HomeController::class, 'payment'])->name('payment');
@@ -21,15 +17,12 @@ Route::get('/crud', [CrudController::class, 'index'])->name('crud');
 Route::get('Adminhome', [HomeController::class, 'Adminhome'])->name('Adminhome');
 Route::get('Addseat', [HomeController::class, 'Addseat'])->name('Addseat');
 Route::get('ViewRes', [HomeController::class, 'ViewRes'])->name('ViewRes');
-
 Route::prefix('/crud')->group(function () {
     Route::get('/', [CrudController::class, 'index'])->name('crud');
     Route::post('/store', [CrudController::class, 'store'])->name('crud.store');
 });
-
 Route::post('loginProcess', [AuthController::class, 'loginProcess'])->name('loginProcess');
 Route::post('AdminloginProcess', [AuthController::class, 'AdminloginProcess'])->name('AdminloginProcess');
-
 Route::get('/seats', [SeatController::class, 'index'])->name('seats.index');
 Route::post('/seats/book', [SeatController::class, 'bookSeat'])->name('seats.book');
 Route::post('/seats', [SeatController::class, 'store'])->name('seats.store');
@@ -37,8 +30,6 @@ Route::get('viewuser', [SeatController::class, 'viewuser'])->name('viewuser');
 Route::get('/booked-seats', [SeatController::class, 'viewBookedSeats'])->name('booked.seats');
 Route::get('/bookedUserSeats', [SeatController::class, 'viewBookedLoggedUserSeats'])->name('userbooked.seats');
 Route::patch('/seats/{seatId}/cancel', [SeatController::class, 'cancelSeatReservation'])->name('seats.cancel');
-
-
 Route::post('/store', [AttendanceController::class, 'store'])->name('attendance.store');
 Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
 Route::get('Viewattendance', [HomeController::class, 'Viewattendance'])->name('Viewattendance');
@@ -48,8 +39,6 @@ Route::get('auth/facebook', [LoginController::class, 'redirectToFacebook'])->nam
 Route::get('auth/facebook/callback', [LoginController::class, 'handleFacebookCallback']);
 Route::get('auth/google',[GoogleAuthController::class,'redirect'])->name('google-auth');
 Route::get('auth/google/call-back',[GoogleAuthController::class,'callbackGoogle']);
-
-
 Route::get('auth/facebook', [FacebookController::class, 'facebookpage'])->name('auth.facebook');
 Route::get('auth/facebook/callback', [FacebookController::class, 'handleFacebookCallback']);
 
